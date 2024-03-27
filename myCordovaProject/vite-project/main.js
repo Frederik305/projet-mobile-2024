024d85b1f2566e7b757f298ca5aa693f44d5701d
Logan <129766393+Logansenechal@users.noreply.github.com>
2024-03-27 16:17:02 -0400

    Update main.js

--- myCordovaProject/vite-project/main.js
+++ myCordovaProject/vite-project/main.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -11,20 +11,19 @@ document.body.appendChild( renderer.domElement );
 
 scene.background=new THREE.Color(0xbfe3dd);
 
-const geometry = new THREE.BoxGeometry( 2, 3, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+
+camera.position.set(0,200,600);
+
 let voiture;
 
 const loader = new GLTFLoader();
 
 loader.load( 'public/Sports.glb', ( gltf )=> {
 	voiture=gltf.scene
+	voiture.position.x =0;
 	scene.add( voiture );
-
+	renderer.render( scene, camera );
 }, undefined, function ( error ) {
 
 	console.error( error );
@@ -36,15 +35,15 @@ function resizeModel(scale) {
         voiture.scale.set(scale, scale, scale);
     }
 }
-resizeModel()
+
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.002;
+	voiture.rotation.x += 0.01;
+	voiture.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+	
 }
 
-animate();
+//animate();

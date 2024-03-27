b11e6580c01f5e56fa902aa17a7330e7ec0f4644
Logan <129766393+Logansenechal@users.noreply.github.com>
2024-03-27 15:20:57 -0400

    Update main.js

--- myCordovaProject/vite-project/main.js
+++ myCordovaProject/vite-project/main.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -15,6 +17,26 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+let voiture;
+
+const loader = new GLTFLoader();
+
+loader.load( 'public/Sports.glb', ( gltf )=> {
+	voiture=gltf.scene
+	scene.add( voiture );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+function resizeModel(scale) {
+    if (voiture) {
+        voiture.scale.set(scale, scale, scale);
+    }
+}
+resizeModel()
 
 function animate() {
 	requestAnimationFrame( animate );

eb0f57752669a1d7e6f7a8a6586c34734f774ddb
Logan <129766393+Logansenechal@users.noreply.github.com>
2024-03-27 15:04:28 -0400

    ajout de background

--- myCordovaProject/vite-project/main.js
+++ myCordovaProject/vite-project/main.js
@@ -7,8 +7,10 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+scene.background=new THREE.Color(0xbfe3dd);
+
+const geometry = new THREE.BoxGeometry( 2, 3, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -18,7 +20,7 @@ function animate() {
 	requestAnimationFrame( animate );
 
 	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	cube.rotation.y += 0.002;
 
 	renderer.render( scene, camera );
 }
